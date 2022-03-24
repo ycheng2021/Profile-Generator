d8251f59f38f0049080518e3060c3e2aedb2f9d8
Yang Cheng <annacheng09@hotmail.com>
2022-03-23 20:42:19 -0700

    save changes to htmlGen

--- src/htmlGen.js
+++ src/htmlGen.js
@@ -60,7 +60,7 @@ const baseHtml = (htmlData) => `
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>A Basic HTML5 Template</title>
+  <title>Team Profiles</title>
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
   <link rel="stylesheet" href="./css/all.css">
   <link rel="stylesheet" href="./css/style.css">
